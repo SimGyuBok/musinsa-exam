c16737a95e4215185787d5d99a1c6ef3b0a0eb70
DINO-Glen <bokgyu@dinostudio.kr>
2024-08-06 09:14:30 +0900

    init project

---
+++ src/main/java/com/musinsa/example/simgyubok/SimgyubokApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.example.simgyubok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimgyubokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimgyubokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musinsa/example/simgyubok/SimgyubokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.example.simgyubok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimgyubokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
